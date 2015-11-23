89941f5165506603abbf1563dd1c024e1a975783
Christopher Brown <io@henrian.com>
2015-11-22 22:56:18 -0600

    Remove local type_declarations/

--- type_declarations/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
-/// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/request/request.d.ts" />
-/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
