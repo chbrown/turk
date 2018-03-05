dd64ecfd4c7f977d367a699f4b188598225ca215
Christopher Brown <io@henrian.com>
2018-03-04 19:07:44 -0600

    Move CLI logic from bin/turk.ts to index.main; replace with bin/turk stub

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash'
 import * as request from 'request'
 import * as crypto from 'crypto'
+import * as optimist from 'optimist'
 
 export type StringObject = {[index: string]: string}
 
@@ -227,4 +228,69 @@ export class Connection {
   get(operation: string, params: any, callback: (error: Error, result?: any) => void) {
     throw new Error('Not yet implemented')
   }
+}
+
+export function main() {
+  const argvparser = optimist
+  .usage(`Usage: turk <options>
+  turk Operation=SearchQualificationTypes -- query the available search qualification types
+  turk Operation=SendTestEventNotification Notification.1.Destination=https://sqs.us-east-1.amazonaws.com/185747841350/turk_all Notification.1.Transport=SQS Notification.1.Version=2006-05-05 Notification.1.EventType=HITReviewable -- query the available search qualification types
+  turk Operation=GetAccountBalance -- get your account balance`)
+  .options({
+    accessKeyId: {
+      describe: 'AWS Access Key',
+      default: process.env.AWS_ACCESS_KEY_ID,
+    },
+    secretAccessKey: {
+      describe: 'AWS Secret Key',
+      default: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+    production: {
+      describe: 'Use the production environment (default: sandbox)',
+      type: 'boolean',
+      alias: 'p',
+    },
+    help: {
+      describe: 'print this help message',
+      type: 'boolean',
+      alias: 'h',
+    },
+    verbose: {
+      describe: 'print extra output',
+      type: 'boolean',
+    },
+    version: {
+      describe: 'print version',
+      type: 'boolean',
+    },
+  })
+
+  const argv = argvparser.argv
+
+  if (argv.version) {
+    console.log(require('./package.json').version)
+  }
+  else if (argv.help) {
+    optimist.showHelp()
+  }
+  else {
+    const params = splitStrings(argv._)
+    const environment = argv.production ? Environment.production : Environment.sandbox
+    const account = new Account(argv.accessKeyId, argv.secretAccessKey)
+    const connection = account.createConnection(environment)
+    if (isPostParameters(params)) {
+      connection.post(params, (err, xml) => {
+        if (err) return console.error(err)
+
+        console.log(xml)
+      })
+    }
+    else {
+      throw new Error('"Operation" field must be provided in params string.');
+    }
+  }
+}
+
+if (require.main === module) {
+  main()
 }
